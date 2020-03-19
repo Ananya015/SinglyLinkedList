68e60e0b4e45cde39db42ed97cfac9fe94b7cb89
Ananya015 <jainananya78@gmail.com>
2020-03-20 00:21:39 +0530

    complete method getNode

--- src/definition/SinglyLinkedList.java
+++ src/definition/SinglyLinkedList.java
@@ -19,7 +19,11 @@ public class SinglyLinkedList<E> {
     }
 
     public Node<E> getNode(int index){
-
+        Node<E> response=head;
+        for(int i=0;i<index;i++){
+            response=response.getNext ();
+        }
+      return response;
     }
     public void add(int index,E item){
         if(index<0 || index>size) {
