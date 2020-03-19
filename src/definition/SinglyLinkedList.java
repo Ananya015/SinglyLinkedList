a609c98cf61053863a3c3d2393d5d544a6694586
Ananya015 <jainananya78@gmail.com>
2020-03-19 23:52:24 +0530

    generate getter

--- src/definition/SinglyLinkedList.java
+++ src/definition/SinglyLinkedList.java
@@ -6,6 +6,13 @@ private static class Node<E>{
     private E data;
     private Node<E> next;
 
+    public E getData() {
+        return data;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
 }
 
 
